bdb0a1d0f576e4ed829d635c2d958810866268d3
Aarav <aaravk061@gmail.com>
2026-06-16 17:44:50 +0530

    Implement JWT token generation in login serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import User
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.tokens import RefreshToken
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
@@ -15,4 +17,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.Serializer):
     email=serializers.EmailField()
     password=serializers.CharField(write_only=True)
-    
+    def validate(self,attrs):
+        attrs=attrs['email']
+        password=password['password']
+        
